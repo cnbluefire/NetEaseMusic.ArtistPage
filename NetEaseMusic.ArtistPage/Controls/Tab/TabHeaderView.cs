7f034a29748786abc26b7ef714ef59a3e1cf9071
blue-fire <909683964@qq.com>
2018-12-21 20:44:07 +0800

    添加指示器宽度高度

--- NetEaseMusic.ArtistPage/Controls/Tab/TabHeaderView.cs
+++ NetEaseMusic.ArtistPage/Controls/Tab/TabHeaderView.cs
@@ -37,12 +37,14 @@ namespace NetEaseMusic.ArtistPage.Controls.Tab
         CompositionPropertySet PropSet;
         SpriteVisual Indicator;
         Visual ScrollViewerVisual;
-        ExpressionAnimation ProgressExpression;
-        ExpressionAnimation IndicatorOffsetExpression;
-        ExpressionAnimation IndicatorSizeExpression;
-        ExpressionAnimation BaseOffsetXExpression;
-        ExpressionAnimation PropertySetWidthExpression;
-        ExpressionAnimation PropertySetOffsetXExpression;
+        ExpressionAnimation Indicator_OffsetExpression;
+        ExpressionAnimation Indicator_SizeExpression;
+        ExpressionAnimation PropertySet_ProgressExpression;
+        ExpressionAnimation PropertySet_ActualWidthExpression;
+        ExpressionAnimation PropertySet_ActualHeightExpression;
+        ExpressionAnimation PropertySet_BaseOffsetXExpression;
+        ExpressionAnimation PropertySet_WidthExpression;
+        ExpressionAnimation PropertySet_OffsetXExpression;
 
         #endregion Field
 
@@ -103,14 +105,20 @@ namespace NetEaseMusic.ArtistPage.Controls.Tab
             {
                 PropSet = Compositor.CreatePropertySet();
                 PropSet.InsertScalar("Progress", 0f);
+                PropSet.InsertScalar("ProgressWithDelay", 0f);
+                PropSet.InsertScalar("ProgressWithoutDelay", 0f);
                 PropSet.InsertScalar("BaseOffsetX", 0f);
                 PropSet.InsertScalar("OffsetX", 0f);
                 PropSet.InsertScalar("Width", 0f);
+                PropSet.InsertScalar("ActualWidth", 0f);
+                PropSet.InsertScalar("ActualHeight", 0f);
                 PropSet.InsertScalar("LeftWidth", 0f);
                 PropSet.InsertScalar("RightWidth", 0f);
                 PropSet.InsertScalar("SelectedIndex", 0f);
                 PropSet.InsertScalar("NowOffsetX", 0f);
                 PropSet.InsertScalar("NowWidth", 0f);
+                PropSet.InsertScalar("SuggestionWidth", Convert.ToSingle(IndicatorWidth));
+                PropSet.InsertScalar("SuggestionHeight", Convert.ToSingle(IndicatorHeight));
                 PropSet.InsertScalar("TabsWidth", Convert.ToSingle(_TabsWidth));
 
                 Indicator = Compositor.CreateSpriteVisual();
@@ -125,49 +133,63 @@ namespace NetEaseMusic.ArtistPage.Controls.Tab
         {
             if (Indicator != null && ScrollViewerVisual != null && ScrollPropertySet != null)
             {
-                if (ProgressExpression == null)
+                if (Indicator_OffsetExpression == null)
                 {
-                    ProgressExpression = Compositor.CreateExpressionAnimation("-((scroll.Translation.X + (propset.SelectedIndex * propset.TabsWidth)) / propset.TabsWidth)");
-                    ProgressExpression.SetReferenceParameter("scroll", ScrollPropertySet);
-                    ProgressExpression.SetReferenceParameter("propset", PropSet);
-                    PropSet.StartAnimation("Progress", ProgressExpression);
+                    Indicator_OffsetExpression = Compositor.CreateExpressionAnimation("Vector3(propset.BaseOffsetX + propset.OffsetX + (propset.Width - propset.ActualWidth) / 2, host.Size.Y - target.Size.Y, 0)");
+                    Indicator_OffsetExpression.SetReferenceParameter("propset", PropSet);
+                    Indicator_OffsetExpression.SetReferenceParameter("host", ScrollViewerVisual);
+                    Indicator_OffsetExpression.SetReferenceParameter("target", Indicator);
+                    Indicator.StartAnimation("Offset", Indicator_OffsetExpression);
                 }
 
-                if (IndicatorOffsetExpression == null)
+                if (Indicator_SizeExpression == null)
                 {
-                    IndicatorOffsetExpression = Compositor.CreateExpressionAnimation("Vector3(propset.BaseOffsetX + propset.OffsetX + 5, host.Size.Y - target.Size.Y, 0)");
-                    IndicatorOffsetExpression.SetReferenceParameter("propset", PropSet);
-                    IndicatorOffsetExpression.SetReferenceParameter("host", ScrollViewerVisual);
-                    IndicatorOffsetExpression.SetReferenceParameter("target", Indicator);
-                    Indicator.StartAnimation("Offset", IndicatorOffsetExpression);
+                    Indicator_SizeExpression = Compositor.CreateExpressionAnimation("Vector2(propset.ActualWidth, propset.ActualHeight)");
+                    Indicator_SizeExpression.SetReferenceParameter("propset", PropSet);
+                    Indicator.StartAnimation("Size", Indicator_SizeExpression);
                 }
 
-                if (IndicatorSizeExpression == null)
+                if (PropertySet_ProgressExpression == null)
                 {
-                    IndicatorSizeExpression = Compositor.CreateExpressionAnimation("Vector2(propset.Width - 10,4)");
-                    IndicatorSizeExpression.SetReferenceParameter("propset", PropSet);
-                    Indicator.StartAnimation("Size", IndicatorSizeExpression);
+                    PropertySet_ProgressExpression = Compositor.CreateExpressionAnimation("-((scroll.Translation.X + (propset.SelectedIndex * propset.TabsWidth)) / propset.TabsWidth)");
+                    PropertySet_ProgressExpression.SetReferenceParameter("scroll", ScrollPropertySet);
+                    PropertySet_ProgressExpression.SetReferenceParameter("propset", PropSet);
+                    PropSet.StartAnimation("Progress", PropertySet_ProgressExpression);
                 }
 
-                if (BaseOffsetXExpression == null)
+                if (PropertySet_ActualWidthExpression == null)
                 {
-                    BaseOffsetXExpression = Compositor.CreateExpressionAnimation("headerScroll.Translation.X");
-                    BaseOffsetXExpression.SetReferenceParameter("headerScroll", HeaderScrollPropertySet);
-                    PropSet.StartAnimation("BaseOffsetX", BaseOffsetXExpression);
+                    PropertySet_ActualWidthExpression = Compositor.CreateExpressionAnimation("propset.SuggestionWidth == 0 ? (propset.Width - 10) : (min(propset.SuggestionWidth, propset.Width - 10))");
+                    PropertySet_ActualWidthExpression.SetReferenceParameter("propset", PropSet);
+                    PropSet.StartAnimation("ActualWidth", PropertySet_ActualWidthExpression);
                 }
 
-                if (PropertySetWidthExpression == null)
+                if (PropertySet_ActualHeightExpression == null)
                 {
-                    PropertySetWidthExpression = Compositor.CreateExpressionAnimation("propset.Progress > 0 ? (propset.NowWidth + (propset.RightWidth - propset.NowWidth) * propset.Progress) : (propset.NowWidth - (propset.LeftWidth - propset.NowWidth) * propset.Progress)");
-                    PropertySetWidthExpression.SetReferenceParameter("propset", PropSet);
-                    PropSet.StartAnimation("Width", PropertySetWidthExpression);
+                    PropertySet_ActualHeightExpression = Compositor.CreateExpressionAnimation("propset.SuggestionHeight == 0 ? (4) : (max(propset.SuggestionHeight, 4))");
+                    PropertySet_ActualHeightExpression.SetReferenceParameter("propset", PropSet);
+                    PropSet.StartAnimation("ActualHeight", PropertySet_ActualHeightExpression);
                 }
 
-                if (PropertySetOffsetXExpression == null)
+                if (PropertySet_BaseOffsetXExpression == null)
                 {
-                    PropertySetOffsetXExpression = Compositor.CreateExpressionAnimation("propset.Progress > 0 ? (propset.NowOffsetX + propset.Progress * propset.NowWidth) : (propset.NowOffsetX + propset.Progress * propset.LeftWidth)");
-                    PropertySetOffsetXExpression.SetReferenceParameter("propset", PropSet);
-                    PropSet.StartAnimation("OffsetX", PropertySetOffsetXExpression);
+                    PropertySet_BaseOffsetXExpression = Compositor.CreateExpressionAnimation("headerScroll.Translation.X");
+                    PropertySet_BaseOffsetXExpression.SetReferenceParameter("headerScroll", HeaderScrollPropertySet);
+                    PropSet.StartAnimation("BaseOffsetX", PropertySet_BaseOffsetXExpression);
+                }
+
+                if (PropertySet_WidthExpression == null)
+                {
+                    PropertySet_WidthExpression = Compositor.CreateExpressionAnimation("propset.Progress > 0 ? (propset.NowWidth + (propset.RightWidth - propset.NowWidth) * propset.Progress) : (propset.NowWidth - (propset.LeftWidth - propset.NowWidth) * propset.Progress)");
+                    PropertySet_WidthExpression.SetReferenceParameter("propset", PropSet);
+                    PropSet.StartAnimation("Width", PropertySet_WidthExpression);
+                }
+
+                if (PropertySet_OffsetXExpression == null)
+                {
+                    PropertySet_OffsetXExpression = Compositor.CreateExpressionAnimation("propset.Progress > 0 ? (propset.NowOffsetX + propset.Progress * propset.NowWidth) : (propset.NowOffsetX + propset.Progress * propset.LeftWidth)");
+                    PropertySet_OffsetXExpression.SetReferenceParameter("propset", PropSet);
+                    PropSet.StartAnimation("OffsetX", PropertySet_OffsetXExpression);
                 }
             }
         }
@@ -186,6 +208,15 @@ namespace NetEaseMusic.ArtistPage.Controls.Tab
             if (Indicator != null)
             {
                 Indicator.Brush = Window.Current.Compositor.CreateColorBrush(IndicatorColor);
+            }
+        }
+
+        private void UpdateIndicatorSize()
+        {
+            if (PropSet != null)
+            {
+                PropSet.InsertScalar("SuggestionWidth", Convert.ToSingle(IndicatorWidth));
+                PropSet.InsertScalar("SuggestionHeight", Convert.ToSingle(IndicatorHeight));
             }
         }
 
@@ -292,6 +323,18 @@ namespace NetEaseMusic.ArtistPage.Controls.Tab
             set { SetValue(IndicatorColorProperty, value); }
         }
 
+        public double IndicatorWidth
+        {
+            get { return (double)GetValue(IndicatorWidthProperty); }
+            set { SetValue(IndicatorWidthProperty, value); }
+        }
+
+        public double IndicatorHeight
+        {
+            get { return (double)GetValue(IndicatorHeightProperty); }
+            set { SetValue(IndicatorHeightProperty, value); }
+        }
+
         public static readonly DependencyProperty IndicatorColorProperty =
             DependencyProperty.Register("IndicatorColor", typeof(Color), typeof(TabHeaderView), new PropertyMetadata(null, (s, a) =>
             {
@@ -306,6 +349,37 @@ namespace NetEaseMusic.ArtistPage.Controls.Tab
                     }
                 }
             }));
+
+        public static readonly DependencyProperty IndicatorWidthProperty =
+            DependencyProperty.Register("IndicatorWidth", typeof(double), typeof(TabHeaderView), new PropertyMetadata(0d, (s, a) =>
+            {
+                if (a.NewValue != a.OldValue)
+                {
+                    if (s is TabHeaderView sender)
+                    {
+                        if (sender._IsLoaded)
+                        {
+                            sender.UpdateIndicatorSize();
+                        }
+                    }
+                }
+            }));
+
+        public static readonly DependencyProperty IndicatorHeightProperty =
+            DependencyProperty.Register("IndicatorHeight", typeof(double), typeof(TabHeaderView), new PropertyMetadata(0d, (s, a) =>
+            {
+                if (a.NewValue != a.OldValue)
+                {
+                    if (s is TabHeaderView sender)
+                    {
+                        if (sender._IsLoaded)
+                        {
+                            sender.UpdateIndicatorSize();
+                        }
+                    }
+                }
+            }));
+
         #endregion Dependency Property
     }
 }
